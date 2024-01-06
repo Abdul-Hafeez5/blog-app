cb77986c35c3bb50bf3d060ae5841b2b67655b57
Abdul-Hafeez5 <hafeezrahimoon531@gmail.com>
2024-01-06 22:42:48 +0500

    Update App

--- api/index.js
+++ api/index.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const cors = require("cors");
-const { default: mongoose } = require("mongoose");
+const { mongoose } = require("mongoose");
+const User = require("./models/User");
+const Post = require("./models/Post");
 const bcrypt = require("bcryptjs");
+const app = express();
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
 const multer = require("multer");
-const fs = require("fs");
 const uploadmiddleware = multer({ dest: "uploads/" });
-const User = require("./models/user");
-const Post = require("./models/Post");
-const app = express();
+const fs = require("fs");
 const port = 4000;
 
 const salt = bcrypt.genSaltSync(10);
@@ -18,6 +18,7 @@ const secret = "ghfkgfkvdhjfdkfdkfd";
 app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(express.json());
 app.use(cookieParser());
+app.use("/uploads", express.static(__dirname + "/uploads"));
 
 mongoose
   .connect(
@@ -43,20 +44,47 @@ app.post("/register", async (req, res) => {
   }
 });
 
+// app.post("/login", async (req, res) => {
+//   const { username, password } = req.body;
+//   const UserDoc = await User.findOne({ username });
+//   const isPassword = bcrypt.compareSync(password, UserDoc.password);
+//   if (isPassword) {
+//     jwt.sign({ username, id: UserDoc._id }, secret, {}, (err, token) => {
+//       if (err) throw err;
+//       res.cookie("token", token).json({
+//         id: UserDoc._id,
+//         username,
+//       });
+//     });
+//   } else {
+//     res.status(400).json("Invalid username or password");
+//   }
+// });
+
 app.post("/login", async (req, res) => {
   const { username, password } = req.body;
-  const UserDoc = await User.findOne({ username });
-  const isPassword = bcrypt.compareSync(password, UserDoc.password);
-  if (isPassword) {
-    jwt.sign({ username, id: UserDoc._id }, secret, {}, (err, token) => {
-      if (err) throw err;
-      res.cookie("token", token).json({
-        id: UserDoc._id,
-        username,
+  try {
+    const UserDoc = await User.findOne({ username });
+
+    if (!UserDoc) {
+      return res.status(400).json("Invalid username or password");
+    }
+
+    const isPassword = bcrypt.compareSync(password, UserDoc.password);
+
+    if (isPassword) {
+      jwt.sign({ username, id: UserDoc._id }, secret, {}, (err, token) => {
+        if (err) throw err;
+        res.cookie("token", token).json({
+          id: UserDoc._id,
+          username,
+        });
       });
-    });
-  } else {
-    res.status(400).json("Invalid username or password");
+    } else {
+      res.status(400).json("Invalid username or password");
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Server error" });
   }
 });
 
@@ -92,8 +120,62 @@ app.post("/post", uploadmiddleware.single("file"), async (req, res) => {
   });
 });
 
+app.put("/post", uploadmiddleware.single("file"), async (req, res) => {
+  let newPath = null;
+  if (req.file) {
+    const { originalname, path } = req.file;
+    const parts = originalname.split(".");
+    const ext = parts[parts.length - 1];
+    newPath = path + "." + ext;
+    fs.renameSync(path, newPath);
+  }
+
+  const { token } = req.cookies;
+  jwt.verify(token, secret, {}, async (err, info) => {
+    if (err) throw err;
+    const { id, title, summary, content } = req.body;
+    try {
+      const postDoc = await Post.findById(id);
+      if (!postDoc) {
+        return res.status(404).json("Post not found");
+      }
+
+      const isAuthor =
+        JSON.stringify(postDoc.author) === JSON.stringify(info.id);
+      if (!isAuthor) {
+        return res.status(403).json("You are not the author of this post");
+      }
+
+      const updatedFields = {
+        title,
+        summary,
+        content,
+        cover: newPath ? newPath : postDoc.cover,
+      };
+
+      const updatedPost = await Post.findByIdAndUpdate(id, updatedFields, {
+        new: true,
+      });
+      res.json(updatedPost);
+    } catch (error) {
+      res.status(500).json({ error: "Server error" });
+    }
+  });
+});
+
 app.get("/post", async (req, res) => {
-  res.json(await Post.find().populate("author", ["username"]));
+  res.json(
+    await Post.find()
+      .populate("author", ["username"])
+      .sort({ createdAt: -1 })
+      .limit(20)
+  );
+});
+
+app.get("/post/:id", async (req, res) => {
+  const { id } = req.params;
+  const postDoc = await Post.findById(id).populate("author", ["username"]);
+  res.json(postDoc);
 });
 
 app.listen(port, () => {
